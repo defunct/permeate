9d859c1f5ff7950e8557f5131d0ef620054576b9
Alan Gutierrez <alan@blogometer.com>
2010-05-09 19:20:10 -0500

    Deleted API package from tests.

--- src/test/java/com/goodworkalan/infuse/PathTest.java
+++ src/test/java/com/goodworkalan/infuse/PathTest.java
@@ -10,6 +10,13 @@ import org.testng.annotations.Test;
 public class PathTest
 {
     @Test
+    public void test() throws PathException 
+    {
+        Path path = new Path("foo[1][2].bar", false);
+        assertEquals(path.withoutIndexes(), "foo.bar");
+    }
+    
+    @Test
     public void pathException()
     {
         try
