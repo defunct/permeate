0ab9d967d20c72dec1df0cd83beeb73e32fa841f
Alan Gutierrez <alan@blogometer.com>
2010-05-16 22:57:36 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/permeate/mix/PermeateProject.java
+++ src/mix/java/com/goodworkalan/permeate/mix/PermeateProject.java
@@ -4,17 +4,25 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class PermeateProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Permeate.
+ *
+ * @author Alan Gutierrez
+ */
+public class PermeateProject implements ProjectModule {
+    /**
+     * Build the project definition for Permeate.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.permeate/permeate/0.1")
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("org.mockito/mockito-core/1.6")
-                        .end()
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
