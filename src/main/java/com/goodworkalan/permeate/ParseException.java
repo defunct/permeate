599bcb53ba03c7e64dc74c3b601b8e38b9925cdf
Alan Gutierrez <alan@blogometer.com>
2010-06-27 14:57:31 -0500

    Javadoc TODOs.

--- src/main/java/com/goodworkalan/permeate/ParseException.java
+++ src/main/java/com/goodworkalan/permeate/ParseException.java
@@ -3,18 +3,22 @@ package com.goodworkalan.permeate;
 // FIXME Rename to path parse exception.
 public class ParseException extends PathException
 {
+    // TODO Document.
     private static final long serialVersionUID = 1L;
 
+    // TODO Document.
     public ParseException(int code)
     {
         super(code);
     }
     
+    // TODO Document.
     public ParseException(int code, Throwable cause)
     {
         super(code, cause);
     }
     
+    // TODO Document.
     public ParseException add(Object argument)
     {
         arguments.add(argument);
